c88d0994a673771e3e8557998af9ab171f0a5fbf
talhakhalid-tech <talhakhalid0323@gmail.com>
2020-07-28 16:47:15 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux'
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from './reducers'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 ReactDOM.render(
-  <Provider store={createStore(reducer, compose(composeEnhancers(applyMiddleware(thunk))))}>
+  <Provider store={createStore(reducer, applyMiddleware(thunk))}>
     <App />
   </Provider >,
   document.getElementById('root')
